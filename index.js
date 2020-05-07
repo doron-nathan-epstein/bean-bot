8e06ef3f11ea455db8894af4bb3fdfb0977e0e3a
Doron Epstein <jimbeanzn@gmail.com>
2020-05-07 12:45:46 +0200

    feat: try out voice event

--- index.js
+++ index.js
@@ -9,4 +9,20 @@ client.on("message", msg => {
     msg.reply("Pong! by Bean")
   }
 })
+client.on("voiceStateUpdate", (oState, nState) => {
+  let newUserChannel = nState.voiceChannel
+  let oldUserChannel = oState.voiceChannel
+
+  if(newUserChannel != oldUserChannel) {
+    if(oldUserChannel === undefined && newUserChannel !== undefined) {
+      console.log(nState.member.displayName + " has joined the channel.")
+      // User Joins a voice channel
+
+    } else if(newUserChannel === undefined){
+
+      console.log(nState.member.displayName + " has left the channel.")
+
+    }
+  }
+})
 client.login(process.env.BOT_TOKEN)
