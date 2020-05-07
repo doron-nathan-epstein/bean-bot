017483398c6aa9f5cb5b14f42a50f7ae699c07d4
Doron Epstein <jimbeanzn@gmail.com>
2020-05-07 20:04:39 +0200

    feat: get join and leave logic right

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 require("dotenv").config()
 const Discord = require("discord.js")
 const client = new Discord.Client()
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
+
 client.on("message", msg => {
   if (msg.content === "!mb ping") {
     msg.reply("Pong! by Bean")
   }
 })
-client.on("voiceStateUpdate", (oState, nState) => {
-  let newUserChannel = nState.voiceChannel
-  let oldUserChannel = oState.voiceChannel
 
-  if(newUserChannel != oldUserChannel) {
-    if(oldUserChannel === undefined && newUserChannel !== undefined) {
-      console.log(nState.member.displayName + " has joined the channel.")
-      // User Joins a voice channel
-
-    } else if(newUserChannel === undefined){
-
-      console.log(nState.member.displayName + " has left the channel.")
-
+client.on("voiceStateUpdate", async (oldState, newState) => {
+  if(oldState.channelID !== newState.channelID) {
+    if(newState.channel !== null) {
+      let connection = await newState.channel.join();
+      //connection.play();
+      console.log(newState.channel.name + " - " + newState.member.displayName + " has joined the channel.")
+    }
+    if(oldState.channel !== null) {
+      console.log(oldState.channel.name + " - " + oldState.member.displayName + " has left the channel.")
     }
   }
 })
+
 client.login(process.env.BOT_TOKEN)
