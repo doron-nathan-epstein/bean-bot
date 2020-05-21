6e3e9cd4e714922c871618812c256e812f910b67
Doron Epstein <jimbeanzn@gmail.com>
2020-05-21 19:28:06 +0200

    fix: prefix undefined when no args present

--- events/message.js
+++ events/message.js
@@ -27,7 +27,7 @@ module.exports = async (client, message) => {
     let reply = `You didn't provide any arguments, ${message.author}!`;
 
     if (command.usage) {
-      reply += `\nThe proper usage would be: \`${prefix} ${command.name} ${command.usage}\``;
+      reply += `\nThe proper usage would be: \`${PREFIX} ${command.name} ${command.usage}\``;
     }
 
     return message.channel.send(reply);
