9fe120cf5227e894da627b62c3a2715b8a8f68ad
Doron Epstein <jimbeanzn@gmail.com>
2020-05-21 19:03:28 +0200

    feat: send image to user

--- commands/uwu.js
+++ commands/uwu.js
@@ -3,8 +3,9 @@ const getUserFromMention = require("../utils/userExtensions.js");
 module.exports = {
   name: "uwu",
   description: "Ask me to send an UwU to a user.",
+  guildOnly: true,
   args: true,
-  usage: "[user]",
+  usage: "[@user]",
   async execute(message, args) {
     let user = getUserFromMention(message.client, args[0]);
     if (!user) {
@@ -13,10 +14,12 @@ module.exports = {
       );
     }
 
-    message.channel.send(`${message.author}, your UwU has been sent to <@${user.id}>.`)
-
-    return user.send(
-      `<@${message.author.id}> has sent you an UwU.`
+    message.channel.send(
+      `${message.author}, your UwU has been sent to <@${user.id}>.`
     );
+
+    return user.send(`<@${message.author.id}> has sent you an UwU.`, {
+      files: ["https://www.dailydot.com/wp-content/uploads/2019/10/uwu.jpg"],
+    });
   },
 };
