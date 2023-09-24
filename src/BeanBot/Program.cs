55c08275a7325be29bfcbab589124090251dcf67
Doron Epstein <jimbeanzn@gmail.com>
2023-09-24 21:41:43 +0200

    style: fix indenting

--- src/BeanBot/Program.cs
+++ src/BeanBot/Program.cs
@@ -2,18 +2,19 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 using IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration(config =>
+  .ConfigureAppConfiguration(config =>
+  {
+    //config.AddYamlFile("_config.yml", false);       // Add the config file to IConfiguration variables
+  })
+  .ConfigureLogging(logging =>
+  {
+    logging.SetMinimumLevel(LogLevel.Information);
+    logging.AddSimpleConsole(options =>
     {
-      //config.AddYamlFile("_config.yml", false);       // Add the config file to IConfiguration variables
-    })
-    .ConfigureLogging(logging => {
-      logging.SetMinimumLevel(LogLevel.Information);
-      logging.AddSimpleConsole(options =>
-      {
-        options.SingleLine = true;
-      });
-    })
-    .ConfigureServices(services => services.AddBotServices())
-    .Build();
+      options.SingleLine = true;
+    });
+  })
+  .ConfigureServices(services => services.AddBotServices())
+  .Build();
 
 await host.RunAsync().ConfigureAwait(false);
