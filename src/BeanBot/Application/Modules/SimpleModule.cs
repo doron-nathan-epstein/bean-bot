485793cd3223efcdafb617d7d171348b98241c3f
Doron Epstein <jimbeanzn@gmail.com>
2023-09-30 19:23:30 +0200

    feat: add ping/pong command

--- src/BeanBot/Application/Modules/SimpleModule.cs
+++ src/BeanBot/Application/Modules/SimpleModule.cs
@@ -4,6 +4,9 @@ using Discord.Interactions;
 
 public sealed class SimpleModule : InteractionModuleBase<SocketInteractionContext>
 {
-  [SlashCommand("say", "Make the bot say something.")]
-  public async Task Say(string text) => await ReplyAsync(text).ConfigureAwait(false);
+  [SlashCommand("say", "Make the bot say something.", runMode: RunMode.Async)]
+  public async Task Say(string text) => await ReplyAsync($"Say: {text}").ConfigureAwait(false);
+
+  [SlashCommand("ping", "Make the bot respond with pong.", runMode: RunMode.Async)]
+  public async Task Ping() => await ReplyAsync($"pong!").ConfigureAwait(false);
 }
